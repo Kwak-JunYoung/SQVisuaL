fe215cb3b39204cb35e8f62c32de811fc019cdff
klop0 <klop0@DESKTOP-S45AQN2.mshome.net>
2021-05-02 21:40:57 +0900

    주석으로 달아놨습니다

--- SQVisuaL/src/Main.java
+++ SQVisuaL/src/Main.java
@@ -8,7 +8,24 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		String b;
 		JFrame f = new JFrame();
-		System.out.println("Test1231");
+	    /*JFrame f=new JFrame("SQVisuaL");
+	    JLabel l;
+	    
+	    l = new JLabel("Please select your database type");
+	    l.setBounds(40,20,200,30);
+	    f.add(l);
+	    
+	    JButton MySQL_B=new JButton("MySQL");  
+	    MySQL_B.setBounds(5,130,100,30);  
+	    f.add(MySQL_B);
+	    
+	    JButton SQLite_B=new JButton("SQLite");  
+	    SQLite_B.setBounds(180,130,100,30);  
+	    f.add(SQLite_B);
+	    
+	    f.setSize(300,200);  
+	    f.setLayout(null);  
+	    f.setVisible(true);*/  
 		try {
 			Class.forName("org.sqlite.JDBC");
 			//Class.forName("com.mysql.jdbc.Driver");
