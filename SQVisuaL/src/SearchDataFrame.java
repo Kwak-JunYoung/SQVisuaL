65311f787eb0450b5e785785e73c82f208a8b417
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2021-05-27 04:53:54 +0900

    Update SearchDataFrame.java

--- SQVisuaL/src/SearchDataFrame.java
+++ SQVisuaL/src/SearchDataFrame.java
@@ -9,6 +9,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -22,13 +24,15 @@ public class SearchDataFrame extends JFrame {
    public JTable table;
    public JButton add, delete, cancel, create;
    public DefaultTableModel model;
+   private int row = 0;
    private JScrollPane scrollPane;
    private JComboBox<String> tables;
    private JButton update;
    private JTextField textField;
+
+   
    public SearchDataFrame() {
-      
-      
+	   
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setBounds(100, 100, 450, 300);
       
@@ -41,12 +45,15 @@ public class SearchDataFrame extends JFrame {
       
       delete = new JButton("Delete Constraint");
       panel_1.add(delete);
+      delete.addActionListener(new deleteButtonClickListener());
       
       cancel = new JButton("Cancel");
       panel_1.add(cancel);
+      cancel.addActionListener(new cancelButtonClickListener());
       
       create = new JButton("Create");
       panel_1.add(create);
+      create.addActionListener(new createButtonClickListener());
       
       
       
@@ -69,14 +76,40 @@ public class SearchDataFrame extends JFrame {
       panel_2.add(update);
       
       
-   
-      
-      
 
    }
+   
+   class createButtonClickListener implements ActionListener {
+       public void actionPerformed (ActionEvent e) {
+    	   setVisible(false);
+       }
+       
+    }
+   
+   class cancelButtonClickListener implements ActionListener {
+       public void actionPerformed (ActionEvent e) {
+    	   DefaultTableModel tm = (DefaultTableModel)table.getModel();
+    	   tm.setNumRows(0);
+    	   setVisible(false);
+       }
+       
+    }
+   
+   class deleteButtonClickListener implements ActionListener {
+       public void actionPerformed (ActionEvent e) {
+    	   row = table.getSelectedRow();
+    	   DefaultTableModel tm = (DefaultTableModel)table.getModel();
+    	   if(row>=0 && row < table.getRowCount()) {
+    		   tm.removeRow(row);
+    	   }
+       }
+       
+    }
+   
+		   
+
    
    
    
 
 }
-// ÁÖ¼®¾õµð³ª¤ÃŽä
