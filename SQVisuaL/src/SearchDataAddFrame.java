bcadad2c9a7ed166c4b98f1f19bd437884a9afad
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2021-05-27 22:34:10 +0900

    Now functional

--- SQVisuaL/src/SearchDataAddFrame.java
+++ SQVisuaL/src/SearchDataAddFrame.java
@@ -9,13 +9,18 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JSplitPane;
 import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
 
 public class SearchDataAddFrame extends JFrame {
 
@@ -25,7 +30,7 @@ public class SearchDataAddFrame extends JFrame {
    public JTextField column, constraint;
    private JPanel panel_1;
    private JPanel panel_2;
-   private JComboBox attrList;
+   private JComboBox<String> attrList;
    private JLabel lblNewLabel;
    private JPanel panel_3;
    private JPanel panel_4;
@@ -35,24 +40,27 @@ public class SearchDataAddFrame extends JFrame {
    private JRadioButton lte;
    private JRadioButton neq;
    private JRadioButton eq;
-   private JLabel currentAttrNum;
-   private JTextField textField;
+   private JTextField numComp;
    private JPanel panel_5;
-   private JLabel currentAttrTxt;
    private JPanel panel_6;
    private JRadioButton like;
    private JRadioButton notLike;
-   private JTextField textField_1;
+   private JTextField textComp;
    public SearchDataFrame sdf;
-   public SearchDataAddFrame(SearchDataFrame sdf) {
+   private ArrayList<Pair<String, Boolean>> attrs;
+   private JRadioButton eqtxt;
+   private JRadioButton neqtxt;
+   public SearchDataAddFrame(SearchDataFrame sdf, ArrayList<Pair<String, Boolean>> attrs) {
 
       this.sdf = sdf;
-      setSize(530, 301);
+      this.attrs = attrs;
+      setSize(487, 301);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       contentPane = new JPanel();
       contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
       setContentPane(contentPane);
       contentPane.setLayout(new BorderLayout(0, 0));
+      ButtonGroup g1 = new ButtonGroup(), g2 = new ButtonGroup();
       
       JPanel panel = new JPanel();
       contentPane.add(panel, BorderLayout.SOUTH);
@@ -72,29 +80,29 @@ public class SearchDataAddFrame extends JFrame {
       panel_1.add(panel_3);
       panel_3.setLayout(new GridLayout(1, 3));
       
-      currentAttrTxt = new JLabel("New label");
-      panel_3.add(currentAttrTxt);
-      
       panel_6 = new JPanel();
       panel_3.add(panel_6);
-      panel_6.setLayout(new GridLayout(2, 1));
+      panel_6.setLayout(new GridLayout(4, 1));
       
-      like = new JRadioButton("LIKE");
+      eqtxt = new JRadioButton("Exact match");
+      panel_6.add(eqtxt);
+      
+      neqtxt = new JRadioButton("Doesn't match");
+      panel_6.add(neqtxt);
+      
+      like = new JRadioButton("Like");
       panel_6.add(like);
       
-      notLike = new JRadioButton("NOT LIKE");
+      notLike = new JRadioButton("Not like");
       panel_6.add(notLike);
       
-      textField_1 = new JTextField();
-      panel_3.add(textField_1);
-      textField_1.setColumns(10);
+      textComp = new JTextField();
+      panel_3.add(textComp);
+      textComp.setColumns(10);
       
       panel_4 = new JPanel();
       panel_1.add(panel_4);
       panel_4.setLayout(new GridLayout(1, 3));
-      
-      currentAttrNum = new JLabel("New label");
-      panel_4.add(currentAttrNum);
       
       panel_5 = new JPanel();
       panel_4.add(panel_5);
@@ -106,29 +114,87 @@ public class SearchDataAddFrame extends JFrame {
       neq = new JRadioButton("\u2260");
       panel_5.add(neq);
       
-      lt = new JRadioButton("<");
-      panel_5.add(lt);
+      gt = new JRadioButton(">");
+      panel_5.add(gt);
+      g1.add(gt);
       
       gte = new JRadioButton("\u2265");
       panel_5.add(gte);
+      g1.add(gte);
       
-      gt = new JRadioButton(">");
-      panel_5.add(gt);
+      lt = new JRadioButton("<");
+      panel_5.add(lt);
       
       lte = new JRadioButton("\u2264");
       panel_5.add(lte);
+      g1.add(lte);
       
-      textField = new JTextField();
-      panel_4.add(textField);
-      textField.setColumns(10);
+      numComp = new JTextField();
+      panel_4.add(numComp);
+      numComp.setColumns(10);
       
       panel_2 = new JPanel();
       contentPane.add(panel_2, BorderLayout.NORTH);
       
       lblNewLabel = new JLabel("Apply constraint on:");
       panel_2.add(lblNewLabel);
-      
-      attrList = new JComboBox();
+      attrList = new JComboBox<String>();
+      attrList.addItem("Select an attribute...");
       panel_2.add(attrList);
+      this.setBoxText();
+      g1.add(eq);
+      g1.add(neq);
+      g1.add(lt);
+      
+      g2.add(like);
+      g2.add(notLike);
+      g2.add(eqtxt);
+      g2.add(neqtxt);
+      toggleTxt(false);
+      toggleNum(false);
+      apply.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+    	  
+      });
+      attrList.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED && attrList.getSelectedIndex() != 0) {
+					if(attrs.get(attrList.getSelectedIndex() - 1).getSecond()) {
+						toggleNum(true);
+						toggleTxt(false);
+					} else {
+						toggleNum(false);
+						toggleTxt(true);
+					}
+				}
+			}
+      });
+   }
+   public void toggleTxt(boolean v) {
+	   like.setEnabled(v);
+	   notLike.setEnabled(v);
+	   eqtxt.setEnabled(v);
+	   neqtxt.setEnabled(v);
+	   textComp.setEnabled(v);
+   }
+   public void toggleNum(boolean v) {
+	   eq.setEnabled(v);
+	   neq.setEnabled(v);
+	   gt.setEnabled(v);
+	   gte.setEnabled(v);
+	   lt.setEnabled(v);
+	   lte.setEnabled(v);
+	   numComp.setEnabled(v);
+   }
+   public void setBoxText() {
+	   for(Pair<String, Boolean> p : this.attrs) {
+		   this.attrList.addItem(p.getFirst());
+	   }
    }
 }
